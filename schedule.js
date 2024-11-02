c6b5529b12fb33e4fa73de547878f4d00e646630
Rextouin-R <rattatouille1994@gmail.com>
2024-11-02 15:52:13 +0000

    Update schedule.js

--- schedule.js
+++ schedule.js
@@ -1,5 +1,4 @@
 function scheduleNextRun(defaultDelayInHours = 2, functionToRun) {
-    const printBanner = require("./config/banner");
     const randomDelay = getRandomNumber(600000, 2400000);
     const totalDelay = defaultDelayInHours * 60 * 60 * 1000 + randomDelay;
     const startTime = Date.now();
